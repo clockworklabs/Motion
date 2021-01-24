4010f64c124956d2cc2c9ca834128c1fb7a9f63b
Alessandro Asoni <alessandro@clockworklabs.io>
2021-01-24 18:52:00 +0100

    Use closed form in spring animation

--- Runtime/SpringAnimation.cs
+++ Runtime/SpringAnimation.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using UnityEngine;
 
 namespace Motion
 {
@@ -15,13 +16,22 @@ namespace Motion
                 spring = value;
             }
         }
-        
+
+        // parameters for closed form solution
+        private float Omega { get; set; }
+        private float Zeta { get; set; }
+        private float OmegaZeta { get; set; }
+        private T X0 { get; set; }
+        private T V0 { get; set; }
+        private float ElapsedTime { get; set; } = 0;
+        private DampingProfile dampingProfile { get; set; }
+
         public T Velocity { get; private set; }
 
         internal override void Reset()
         {
             base.Reset();
-            
+
             SetSpring(Spring.Default);
             Velocity = default;
         }
@@ -31,6 +41,10 @@ namespace Motion
             var origin = getter();
             if (origin.Equals(target)) return;
 
+            // Setting parameters
+            X0 = Subtract(target, origin);
+            ElapsedTime = 0;
+
             Setup(getter, setter, origin, target);
         }
 
@@ -38,37 +52,101 @@ namespace Motion
         {
             Spring = spring;
 
+            // Setting parameters
+            Zeta = Spring.damping / (2 * Mathf.Sqrt(Spring.stiffness / Spring.inverseMass));
+            Omega = Mathf.Sqrt(Spring.stiffness * Spring.inverseMass);
+            OmegaZeta = Omega * Zeta;
+            dampingProfile = DampingProfile.Underdamped;
+            if (Mathf.Abs(1 - Zeta) < 0.01f)
+            {
+                dampingProfile = DampingProfile.CriticallyDamped;
+            }
+            else if (Zeta > 1)
+            {
+                dampingProfile = DampingProfile.Overdamped;
+            }
+
             return this;
         }
 
         public SpringAnimation<T> SetInitialVelocity(T velocity)
         {
             if (Started) return this;
-            
+
             Velocity = velocity;
+
+            // Setting parameters
+            V0 = Multiply(Velocity, -1);
 
             return this;
         }
 
-        protected override bool Tick(float deltaTime, ref T value) {
-            // Object position and velocity.
+        protected T TickOverDamped(float t)
+        {
+            var omega2 = Omega * Mathf.Sqrt(Zeta * Zeta - 1.0f);
+            var z1 = -OmegaZeta - omega2;
+            var z2 = -OmegaZeta + omega2;
+            var e1 = Mathf.Exp(z1 * t);
+            var e2 = Mathf.Exp(z2 * t);
+            var c1 = Multiply(Subtract(V0, Multiply(X0, z2)), 1/(-2 * omega2));
+            var c2 = Subtract(X0, c1);
+
+            var x = Add(Multiply(c1, e1), Multiply(c2, e2));
+            Velocity = Add(Multiply(c1, z1 * e1), Multiply(c2, z2 * e2));
+
+            return Subtract(Target, x);
+        }
+
+        protected T TickUnderDamped(float t)
+        {
+            var omega1 = Omega * Mathf.Sqrt(1.0f - Zeta * Zeta);
+            var e = Mathf.Exp(-OmegaZeta * t);
+            var c1 = X0;
+            var c2 = Multiply(Add(V0, Multiply(X0, OmegaZeta)), 1/omega1);
+            var cos = Mathf.Cos(omega1 * t);
+            var sin = Mathf.Sin(omega1 * t);
+            var a = Multiply(Subtract(Multiply(X0, OmegaZeta), Multiply(c2, omega1)), cos);
+            var b = Multiply(Add(Multiply(X0, omega1), Multiply(c2, OmegaZeta)), sin);
+
+            var x = Multiply(Add(Multiply(c1, cos), Multiply(c2, sin)), e);
+            Velocity = Multiply(Add(a, b), e);
+
+            return Subtract(Target, x);
+        }
+
+        protected T TickCriticallyDamped(float t)
+        {
+            var e = Mathf.Exp(-Omega * t);
+
+            var x = Multiply(Add(X0, Multiply(Add(V0, Multiply(X0, Omega)), t)), e);
+            Velocity = Multiply(Add(Multiply(V0, (1 - t * Omega)), Multiply(X0, t * Omega * Omega)), e);
+
+            return Subtract(Target, x);
+        }
+
+        protected override bool Tick(float deltaTime, ref T value)
+        {
             var delta = Subtract(value, Target);
-            if (SqrMagnitude(Velocity) < Spring.sqrRestSpeed && SqrMagnitude(delta) < Spring.sqrRestDelta)
+            var deltaSqrMagnitude = SqrMagnitude(delta);
+            if (SqrMagnitude(Velocity) < Spring.sqrRestSpeed && deltaSqrMagnitude < Spring.sqrRestDelta)
             {
                 value = Target;
                 return true;
             }
-            
-            // Spring stiffness, in kg / s^2
-            var k = -Spring.stiffness;
-            // Damping constant, in kg / s
-            var d = -Spring.damping;
-        
-            var fSpring = Multiply(delta, k);
-            var fDamping = Multiply(Velocity, d);
-            var a = Multiply(Add(fSpring, fDamping), Spring.inverseMass);
-            Velocity = Add(Velocity, Multiply(a, deltaTime));
-            value = Add(value, Multiply(Velocity, deltaTime));
+
+            ElapsedTime += deltaTime;
+            switch(dampingProfile)
+            {
+                case DampingProfile.Overdamped:
+                    value = TickOverDamped(ElapsedTime);
+                    break;
+                case DampingProfile.Underdamped:
+                    value = TickUnderDamped(ElapsedTime);
+                    break;
+                default:
+                    value = TickCriticallyDamped(ElapsedTime);
+                    break;
+            }
 
             return false;
         }
@@ -78,4 +156,12 @@ namespace Motion
         protected abstract T Multiply(T a, float b);
         protected abstract float SqrMagnitude(T a);
     }
+
+    enum DampingProfile
+    {
+        Underdamped,
+        Overdamped,
+        CriticallyDamped
+    }
+
 }
